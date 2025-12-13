1b148999199dc157af34f64366a898ef199ba32e
Aleksandr Tsvyndra <aleks.tsvindra@gmail.com>
2025-12-13 20:36:43 +0300

    Реализовать сервер конфигураций, чтобы сервисы analyzer, aggregator и collector обращались к нему за параметрами конфигурации

--- telemetry/analyzer/src/main/java/ru/yandex/practicum/Analyzer.java
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/Analyzer.java
@@ -3,12 +3,14 @@ package ru.yandex.practicum;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.ConfigurableApplicationContext;
 import ru.yandex.practicum.processor.HubEventProcessor;
 import ru.yandex.practicum.processor.SnapshotProcessor;
 
 @Slf4j
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class Analyzer {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(Analyzer.class, args);
