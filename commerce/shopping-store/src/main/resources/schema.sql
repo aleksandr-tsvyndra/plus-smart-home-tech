b105927bfd5a5d8c93d5998a2e723e4af7e931ba
Aleksandr Tsvyndra <aleks.tsvindra@gmail.com>
2026-01-22 20:42:36 +0300

    Исправить файл schema.sql в сервисе shopping-store

--- commerce/shopping-store/src/main/resources/schema.sql
+++ commerce/shopping-store/src/main/resources/schema.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS products (
     product_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    product_name VARCHAR(50),
-    description VARCHAR(MAX),
+    product_name VARCHAR,
+    description VARCHAR,
     image_src VARCHAR,
-    quantity_state VARCHAR(20),
-    product_state VARCHAR(20),
-    product_category VARCHAR(20),
+    quantity_state VARCHAR,
+    product_state VARCHAR,
+    product_category VARCHAR,
     price NUMERIC
 );
