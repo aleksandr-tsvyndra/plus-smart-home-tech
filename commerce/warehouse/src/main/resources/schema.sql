bf5cc6b6fce93604d15b93abd85cfbd8a24b78b4
Aleksandr Tsvyndra <aleks.tsvindra@gmail.com>
2026-01-22 20:34:16 +0300

    Исправить файлы schema.sql

--- commerce/warehouse/src/main/resources/schema.sql
+++ commerce/warehouse/src/main/resources/schema.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS warehouse_products (
     product_id UUID PRIMARY KEY,
-    width DOUBLE PRECISION,
-    height DOUBLE PRECISION,
-    depth DOUBLE PRECISION,
-    weight DOUBLE PRECISION,
+    width NUMERIC,
+    height NUMERIC,
+    depth NUMERIC,
+    weight NUMERIC,
     fragile BOOLEAN,
     quantity INTEGER
 );
