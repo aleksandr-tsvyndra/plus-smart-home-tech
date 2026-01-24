28e4c80a27f93119803ab1c2e0b82ba3a2d2dc51
Aleksandr Tsvyndra <aleks.tsvindra@gmail.com>
2026-01-24 23:30:36 +0300

    Изменить тип данных поля sort на Set<SortView> в классе PageResponse

--- commerce/shopping-store/src/main/java/ru/yandex/practicum/controller/PageResponse.java
+++ commerce/shopping-store/src/main/java/ru/yandex/practicum/controller/PageResponse.java
@@ -3,43 +3,39 @@ package ru.yandex.practicum.controller;
 import lombok.Getter;
 import lombok.ToString;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 // Данный класс реализован лишь из-за того, что при Postman-тестах объект Page при сериализации
 // в json возвращает неправильный набор полей в объекте Sort
 @Getter @ToString
 public class PageResponse<T> {
-    private final int totalPages;
-    private final long totalElements;
-    private final int size;
     private final List<T> content;
+    private final long totalElements;
+    private final int totalPages;
+    private final boolean last;
+    private final int size;
     private final int number;
-    private final SortView sort;
+    private final Set<SortView> sort = new HashSet<>();
     private final int numberOfElements;
     private final boolean first;
-    private final boolean last;
-    private final PageableView pageable;
-    private final boolean empty;
 
     public PageResponse(Page<T> page) {
-        this.totalPages = page.getTotalPages();
-        this.totalElements = page.getTotalElements();
-        this.size = page.getSize();
         this.content = page.getContent();
+        this.totalElements = page.getTotalElements();
+        this.totalPages = page.getTotalPages();
+        this.last = page.isLast();
+        this.size = page.getSize();
         this.number = page.getNumber();
-        this.sort = new SortView(page.getSort().toList());
         this.numberOfElements = page.getNumberOfElements();
         this.first = page.isFirst();
-        this.last = page.isLast();
-        this.pageable = setPageable(page.getPageable());
-        this.empty = page.isEmpty();
-    }
 
-    private PageableView setPageable(Pageable pageable) {
-        return new PageableView(pageable);
+        for (var order : page.getSort().toSet()) {
+            sort.add(new SortView(List.of(order)));
+        }
     }
 
     @Getter @ToString
@@ -58,25 +54,6 @@ public class PageResponse<T> {
             this.nullHandling = orders.getFirst().getNullHandling();
             this.ascending = orders.getFirst().isAscending();
             this.descending = orders.getFirst().isDescending();
-        }
-    }
-
-    @Getter @ToString
-    static class PageableView {
-        private final int pageNumber;
-        private final int pageSize;
-        private final SortView sort;
-        private final long offset;
-        private final boolean paged;
-        private final boolean unpaged;
-
-        public PageableView(Pageable pageable) {
-            this.pageNumber = pageable.getPageNumber();
-            this.pageSize = pageable.getPageSize();
-            this.sort = new SortView(pageable.getSort().toList());
-            this.offset = pageable.getOffset();
-            this.paged = pageable.isPaged();
-            this.unpaged = pageable.isUnpaged();
         }
     }
 }
